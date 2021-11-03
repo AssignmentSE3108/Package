28547447b29fb1875b73f3ad67342353de4f929f
tanluc2601 <tantanluc08@gmail.com>
2021-11-04 06:59:19 +0700

    Luc - Update cart (no css)

--- cart/cart.js
+++ cart/cart.js
@@ -1,26 +1,37 @@
-var removeCartItemButtons = document.getElementsByClassName('close')
-console.log(removeCartItemButtons)
-for (var i = 0; i < removeCartItemButtons.length; i++) {
+if (document.readyState == 'loading'){
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+function ready() {
+    var removeCartItemButtons = document.getElementsByClassName('close')
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
     var button = removeCartItemButtons[i]
-    button.addEventListener('click', function(event){
-        var buttonClicked = event.target
+    button.addEventListener('click', removeCartItem)
+    }
+}
+
+
+function removeCartItem(event){
+    var buttonClicked = event.target
         buttonClicked.parentElement.parentElement.remove()
         updateCartTotal()
-    })
 }
 
 function updateCartTotal(){
     var cartItemContainer = document.getElementsByClassName('cart-items')[0]
     var cartRows = cartItemContainer.getElementsByClassName('cart-row')
     var total = 0
-    for (var i = 0; i < removeCartItemButtons.length; i++) {
+    for (var i = 0; i < cartRows.length; i++) {
         var cartRow = cartRows[i]
         var priceElement = cartRow.getElementsByClassName('cart-price')[0]
         var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
-        var price = parseFloat(priceElement.innerText.replace('vnđ',''))
+        var price = parseFloat(priceElement.innerText)
         var quantity = quantityElement.value
         total = total + (price * quantity)
     }
     document.getElementsByClassName('cart-sub-total-price')[0].innerText = total
+
 
 }
