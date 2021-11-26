f8fbe6657cf8841af717a31a1a64b3d6f10055a8
vuhah <vuuu0412@gmail.com>
2021-11-26 23:57:04 +0700

    update

--- Herios/login.php
+++ Herios/login.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
-    <link rel="stylesheet" href="../bootstrap.min.css">
-    <link rel="stylesheet" href="./login.css">
+    <link rel="stylesheet" href="/Herios/css/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/Herios/css/login/login.css">
 </head>
 <body>
     <div class="container-fluid pt-5">
