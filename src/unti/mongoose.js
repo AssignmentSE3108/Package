9ea7c5064764ea00c63b8024a2480c827c0b9cfa
vuhah <vuuu0412@gmail.com>
2021-11-28 07:33:39 +0700

    update

--- src/unti/mongoose.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    mutipleMongooToOpject: function (mongooseArray) {
-        return mongooseArray.map(mongooseArray => mongooseArray.toObject());
-    },
-
-    mongooseToObject: function (mongooseArray) {
-        return mongooseArray ? mongooseArray.toObject() : mongooseArray;
-    }
-}
